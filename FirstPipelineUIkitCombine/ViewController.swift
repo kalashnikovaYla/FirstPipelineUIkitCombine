5c99e01672fd47f63c525ed16107fbbfedd0ba58
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-09 14:36:02 +0500

    First commit

--- FirstPipelineUIkitCombine/ViewController.swift
+++ FirstPipelineUIkitCombine/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    var viewModel = FirstPipelineViewModel()
+    let label = UILabel()
+    let textField = UITextField()
+    
+    var anyCanceballe: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.frame = CGRect(x: 250, y: 100, width: 100, height: 50)
+        textField.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
+        textField.placeholder = "Your name"
+        
+        view.addSubview(label)
+        view.addSubview(textField)
+        
+        textField.delegate = self
+        
+        viewModel.$validation
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.text, on: label)
     }
-
-
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        viewModel.name = string
+        return true
+    }
+}
+
+
+class FirstPipelineViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var validation: String? = ""
+    
+    init() {
+        $name
+            .map {$0.isEmpty ? "💔": "❤️‍🔥"}
+            .assign(to: &$validation)
+    }
+}
