525580ddd84d1c0305771f2a9a497cf0fd536191
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-09 14:16:13 +0500

    Initial Commit

---
+++ FirstPipelineUIkitCombine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstPipelineUIkitCombine
+//
+//  Created by sss on 09.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
